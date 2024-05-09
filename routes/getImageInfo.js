e1b8e98e4fbd0799327496aa0daa44e7ea2ec296
sangguiheng <3327563682@qq.com>
2024-05-09 16:43:18 +0800

    搜索图片功能修改

--- routes/getImageInfo.js
+++ routes/getImageInfo.js
@@ -6,25 +6,48 @@ const pool = require('../db');
 router.get('/getImageInfo', (req, res) => {
     
     let select = 'select Image_NAME, Stratum_NAME, Ima_START, Ima_END from ' +
-                 'image_info i left join Stratums s ON i.image_SID = s.Stratum_ID ' +
-                 'where ${req.body.condition}'
+                 'image_info i left join Stratums s ON i.image_SID = s.Stratum_ID '
 
-    if (!req.body.condition) {
-        select = 'select Image_NAME, Stratum_NAME, Ima_START, Ima_END from ' +
-        'image_info i left join Stratums s ON i.image_SID = s.Stratum_ID'
+    let whereConditions = [];
+    let queryParams = [];
+
+    if (req.query.imageSid) {
+        whereConditions.push("Image_SID = ?");
+        queryParams.push(req.query.imageSid);
     }
 
-    pool.query(select, (err, results) => {
+    if (req.query.imageStart) {
+        whereConditions.push("Ima_START > ?");
+        queryParams.push(req.query.imageStart);
+    }
+
+    if (req.query.imageEnd) {
+        whereConditions.push("Ima_END < ?");
+        queryParams.push(req.query.imageEnd);
+    }
+
+    if (req.query.imageDepth) {
+        whereConditions.push("Ima_DEPTH = ?");
+        queryParams.push(req.query.imageDepth);
+    }
+
+    if (req.query.imageType) {
+        whereConditions.push("S_TYPE = ?");
+        queryParams.push(req.query.imageType);
+    }
+
+    if (whereConditions.length > 0) {
+        select += " WHERE " + whereConditions.join(" AND ");
+    }
+
+
+    pool.query(select, queryParams, (err, results) => {
         if (err) {
+            console.error(err)
             return res.status(500).send({ message: '查询数据库出错' });
         }
-        if (results.length > 0) {
-            res.json(results);
-        } else {
-            res.status(404).send({ message: '没有满足条件的图片信息！' });
-        }
+        res.status(200).json(results);
     })
-
 })
 
 module.exports = router
