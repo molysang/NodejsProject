6d1a8a16efaf1f31c20d066b5e5717229a9358cc
sangguiheng <3327563682@qq.com>
2024-05-15 00:15:40 +0800

    希望是最后一次改

--- routes/changeImageInfo.js
+++ routes/changeImageInfo.js
@@ -72,10 +72,81 @@ router.post('/updateSubmit', (req, res) => {
 				res.status(500).send({ message: '输入数据不符合规范' });
 			}
         } else {
+
+            const fs = require('fs');
+            // 原始文件路径
+            const oldPath = '/root/pictures/' + imageNameOld + '.jpg';
+            // 新文件路径
+            const newPath = '/root/pictures/' + imageName + '.jpg';
+
+            fs.rename(oldPath, newPath, (err) => {
+                if (err) {
+                    console.error("重命名过程中发生了问题", err);
+                    return;
+                }
+                console.log("重命名成功");
+            });
+
             res.status(200).send({ message: '修改成功' })
         }
 
     })
+
+})
+
+router.post('deleteImage', (req, res) => {
+
+    const { imageName, uploadNum } = req.body;
+
+    const token = req.headers.authorization.split(' ')[1]; // 获得头部的token
+
+    const deleteSql = 'delete from image_info where IMAGE_NAME = ?';
+
+    jwt.verify(token, SECRET_KEY, (error, decoded) => {
+
+        if (error) {
+            return res.status(401).send({ message: 'Token验证失败' });
+        }
+
+        const account = decoded.userId; //获取ID
+        const select = 'select * from users where U_ACCOUNT = ? AND U_NUM = ?'
+        pool.query(select, [account, uploadNum], (err, results) => {
+            if (err) {
+                console.error('错误信息：', err);
+                return res.status(500).send({ message: '查询数据库出错' });
+            }
+
+            if (results.length > 0) {
+                pool.query(deleteSql, [imageName], (err, results) => {
+                    if (err) {
+                        console.error('错误信息：', err);
+                        return res.status(500).send({ message: '删除错误' });
+                    }
+
+
+                    const fs = require('fs');
+                    // 文件路径
+                    const filePath = '/root/pictures/' + imageName + ".jpg";
+
+                    fs.unlink(filePath, (err) => {
+                        if (err) {
+                            console.error("删除发生错误", err);
+                            return;
+                        }
+                        console.log("文件成功删除");
+                    });
+
+                    
+                    res.status(200).send({ message: '删除成功' });
+                })
+
+            } else {
+                return res.status(500).send({ message: '非本人上传，无法删除' });
+            }
+
+        });
+
+    });
 
 })
 
