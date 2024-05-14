f9c42e77ba6ba1b838c7ffa782178a5e4f2977bd
sangguiheng <3327563682@qq.com>
2024-05-14 20:54:22 +0800

    修改模块

--- routes/changeImageInfo.js
+++ routes/changeImageInfo.js
@@ -2,12 +2,10 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const multer = require('multer');
-const cors = require('cors');
 
-const app = express();
+
 const pool = require('../db');
-
+const SECRET_KEY = "jF44FzpQEuaUOMmYXtWhblIm5yTuMOewmUvrok68bkle-jHTXs2Fr3ddKSqxfCENxZYLLTJSmRgPWOlkJSWClg";
 
 router.get('/showImageInfo', (req, res) => {
 
@@ -28,8 +26,31 @@ router.get('/showImageInfo', (req, res) => {
 
 })
 
-//router.post('/updateImageInfo', (res, req) => {
+router.get('/updateImageInfo', (res, req) => {
 
+    const uploadNum = req.query.uploadNum;
+    const token = req.headers.authorization.split(' ')[1]; // 获得头部的token
 
+	// 解码 Token，提取用户 ID
+	jwt.verify(token, SECRET_KEY, (error, decoded) => {
 
-//})
+        if (error) {
+            return res.status(401).send({ message: 'Token验证失败' });
+        }
+
+        const account = decoded.userId; //获取ID
+        const select = 'select * from users where U_ACCOUNT = ? AND U_NUM = ?'
+        pool.query(select, [account, uploadNum], (err, results) => {
+            if (err) {
+                console.error('错误信息：', err);
+                return res.status(500).send({ message: '查询数据库出错' });
+            }
+
+            res.json(results);
+        });
+
+    });
+
+})
+
+module.exports = router
